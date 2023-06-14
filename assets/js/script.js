b37a4472c44bd3be101515b2c36653ddf0d25ffa
Raúl Palma Kaschel <rpalmakaschel@gmail.com>
2023-06-13 20:25:27 -0400

    Ajustes: Script typed, ajustes bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,36 @@
 let showMenu = false; // Declaro variable
 
 function showHiddenMenu(){ // Funcion para ocultar y mostrar menu
+    console.log("showHiddenMenu function called");
     if (showMenu) {
         document.getElementById("hidden-nav").classList = ""; // Crea la clase "" 
         showMenu = false;
+        console.log("Menu hidden");
     } else {
         document.getElementById("hidden-nav").classList = "show-nav"; // Crea la clase "show-menu" 
-        showMenu = true; 
+        showMenu = true;
+        console.log("Menu shown");
     }
 }
 
 function selection(){ // Funcion oculta menu al seleccionar una opcion
-    document.getElementById("hidden.nav").classList = "";
+    console.log("selection function called");
+    document.getElementById("hidden-nav").classList = "";
     showMenu = false;
+    console.log("Menu hidden");
 }
+
+$(document).ready(function() {
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+      var typed_strings = $('.typed-text').text();
+      var typed = new Typed('.typed-text-output', {
+        strings: typed_strings.split(', '),
+        typeSpeed: 50,
+        backSpeed: 20,
+        smartBackspace: false,
+        loop: true
+      });
+    }
+  });
+  
